056768a552ef7abb4c3fa373d136f7a1a0d1002e
geckse <mail@geckse.de>
2020-01-04 05:17:29 +0100

    mocked backend as websocket server

---
+++ app.js
@@ -0,0 +1,16 @@
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8080 });
+
+/*
+  On Connection
+*/
+wss.on('connection', function connection(ws) {
+
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+
+  ws.send('something');
+});
+
+console.log('Backend Server Online');
