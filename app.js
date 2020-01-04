322dfb05bb4a6c895ee85303696c0e35faa7dd7e
geckse <mail@geckse.de>
2020-01-04 20:37:10 +0100

    added control to all other relay components

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-var devices = [];
+/*
+  Hydroponic IoT Backend
+  Control of several
+*/
+
+var devices = []; // list of connected iot devices
+
+// shared configs
 var liveConfig = {
   power: {
     waterPumps: false,
+    airPumps: false,
+    lamp1: false,
+    lamp2: false,
+    lamp3: false
   }
 };
 
@@ -14,7 +25,40 @@ const app = express();
 app.get('/', (req, res) => {
   res.json(liveConfig);
 });
+/*
+  All Components control (for shutdown, e.q)
+*/
+app.get('/all/off', (req, res) => {
 
+  for (var component in liveConfig.power) {
+    if (Object.prototype.hasOwnProperty.call(liveConfig.power, component)) {
+      liveConfig.power[component] = false;
+    }
+  }
+  devices.forEach(function(d) {
+    // turn pumps off and send to client devices
+    d.send(JSON.stringify(liveConfig));
+
+  });
+  res.json(liveConfig);
+});
+app.get('/all/on', (req, res) => {
+
+  for (var component in liveConfig.power) {
+    if (Object.prototype.hasOwnProperty.call(liveConfig.power, component)) {
+      liveConfig.power[component] = true;
+    }
+  }
+  devices.forEach(function(d) {
+    // turn pumps off and send to client devices
+    d.send(JSON.stringify(liveConfig));
+
+  });
+  res.json(liveConfig);
+});
+/*
+  Water Pump control
+*/
 app.get('/water/off', (req, res) => {
   liveConfig.power.waterPumps = false;
   devices.forEach(function(d) {
@@ -33,6 +77,52 @@ app.get('/water/on', (req, res) => {
   });
   res.json(liveConfig);
 });
+
+/*
+  Air Pump control
+*/
+app.get('/air/off', (req, res) => {
+  liveConfig.power.airPumps = false;
+  devices.forEach(function(d) {
+    // turn pumps off and send to client devices
+    d.send(JSON.stringify(liveConfig));
+
+  });
+  res.json(liveConfig);
+});
+
+app.get('/air/on', (req, res) => {
+  liveConfig.power.airPumps = true;
+  devices.forEach(function(d) {
+    // turn pumps off and send to client devices
+    d.send(JSON.stringify(liveConfig));
+  });
+  res.json(liveConfig);
+});
+
+/*
+  Lamp control
+*/
+for(let i = 1; i <= 3; i++){
+  app.get('/lamp'+i+'/off', (req, res) => {
+    liveConfig.power['lamp'+i] = false;
+    devices.forEach(function(d) {
+      // turn pumps off and send to client devices
+      d.send(JSON.stringify(liveConfig));
+
+    });
+    res.json(liveConfig);
+  });
+
+  app.get('/lamp'+i+'/on', (req, res) => {
+    liveConfig.power['lamp'+i] = true;
+    devices.forEach(function(d) {
+      // turn pumps off and send to client devices
+      d.send(JSON.stringify(liveConfig));
+    });
+    res.json(liveConfig);
+  });
+}
 
 app.listen(8080, () => {
   console.log('Backend API listening on port 8080.');
